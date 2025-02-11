2fc6e93e7e6595e9744a334c3e6f581be9b4d9e4
CasonRichardson <simpletheotter@gmail.com>
2025-02-10 22:54:29 -0500

    Update lab4-4.cpp
    final change

--- lab4-4.cpp
+++ lab4-4.cpp
@@ -1,16 +1,24 @@
 //Lab4-4.cpp - displays the volume of a cylinder
 //Created/revised by <your name> on <current date>
+#include<iostream>
+using namespace std;
+int main() { 
 
-{
-} //end of main function
-cin >> height;
-cout << "Height: ";
-cout << "Radius: ";
-cout << "Volume: " << volume << endl;
 double height   = 0.0;
 double radius   = 0.0;
 double volume   = 0.0;
-int main()
-return 0;
-using namespace std;
+const double PI = 3.14;
+
+
+cout << "Height: ";
+cin >> height;
+cout << "Radius: ";
+cin >> radius; 
+
 volume = PI * radius * radius * height;
+
+cout << "Volume: " << volume << endl;
+
+return 0;
+
+} //end of main function
