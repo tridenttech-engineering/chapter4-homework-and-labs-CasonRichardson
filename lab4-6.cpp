758001d580b69aed9beb49e845ce2c29c29fe783
CasonRichardson <simpletheotter@gmail.com>
2025-02-10 22:59:35 -0500

    Update lab4-6.cpp

--- lab4-6.cpp
+++ lab4-6.cpp
@@ -1,9 +1,8 @@
 //Lab4-6.cpp - displays the area of a triangle
 //Created/revised by <your name> on <current date>
 
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
 	//declare variables
@@ -12,7 +11,7 @@ int main()
 	double area = 0.0;
 
 	//calculate and display area
-	area = base * height / 2;
+	area = base * height / 2.0;
 	cout << "Area: " << area << endl;
 
 	return 0;
